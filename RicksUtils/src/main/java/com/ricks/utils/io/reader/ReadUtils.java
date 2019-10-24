9226c37090328616903676a39f31eed755115606
Jose Osorio Soto <jose_osorio_soto@claro.com.do>
2019-10-24 12:19:02 -0400

    Read Utilities were written. Now we gotta test them out

---
+++ RicksUtils/src/main/java/com/ricks/utils/io/reader/ReadUtils.java
@@ -0,0 +1,65 @@
+package com.ricks.utils.io.reader;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.sql.SQLException;
+import java.util.Objects;
+import java.util.Properties;
+
+public class ReadUtils {
+
+    private ReadUtils() {
+    }
+
+    private static void assertFileIsNotInvalid(final File file) throws IOException {
+        if (Objects.isNull(file))
+            throw new NullPointerException("THE FILE IS NULL");
+        if (!file.exists())
+            throw new FileNotFoundException("FILE DOES NOT EXISTS");
+        if (file.isDirectory())
+            throw new IOException("TRYING TO READ A DIRECTORY AND NOT A FILE");
+    }
+
+    private static void assertInputStreamIsNotInvalid(final InputStream inputStream) {
+        if (Objects.isNull(inputStream))
+            throw new NullPointerException("Trying to Read From a Null InputStream");
+    }
+
+    public static String readToTheEnd(final File file) throws SQLException, IOException {
+        assertFileIsNotInvalid(file);
+        String line = "";
+        final BufferedReader bufferedReader = new BufferedReader(new FileReader(file));
+        while (hasNextLine(bufferedReader)) {
+            line += bufferedReader.readLine();
+        }
+        return line;
+    }
+
+    public static String readToTheEnd(final BufferedReader bReader) throws IOException {
+
+        final StringBuilder content = new StringBuilder();
+        if (Objects.isNull(bReader))
+            return content.append("TRYING TO READ FROM AN EMPTY BUFFER READER").toString();
+        while(hasNextLine(bReader))
+            content.append(bReader.readLine());
+
+        return content.toString();
+    }
+
+    public static Properties readToEndOnProperties(final InputStream inputFlow) throws IOException {
+        assertInputStreamIsNotInvalid(inputFlow);
+        final Properties dotPropertiesFileValues = new Properties();
+        dotPropertiesFileValues.load(inputFlow);
+        return dotPropertiesFileValues;
+    }
+
+    public static boolean hasNextLine(BufferedReader bReader) throws IOException {
+        if (Objects.nonNull(bReader.readLine())) 
+            return true;
+        return false;
+    }
+}
---
+++ RicksUtils/src/main/java/com/ricks/utils/io/writer/RWriting.java
@@ -0,0 +1,16 @@
+package com.ricks.utils.io.writer;
+
+public class RWriting {
+    
+    private RWriting() {}
+
+    public static Boolean writeToTheEnd(final StringBuilder content) {
+        
+        return false;
+    }
+
+    public static Boolean writeToTheEnd(final StringBuffer content) {
+        
+        return false;
+    }
+}
