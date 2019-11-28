4cac4b7d000df33cd258bb42dea84a3128096c8c
Jose Osorio Soto <jose_osorio_soto@claro.com.do>
2019-11-28 17:49:34 -0400

    New Changes to commit

--- MacrosApp/PersistenceLayer/src/main/java/com/macros/persistence/App.java
+++ MacrosApp/PersistenceLayer/src/main/java/com/macros/persistence/App.java
@@ -1,10 +1,20 @@
 package com.macros.persistence;
 
+import java.util.Optional;
+
+import com.macros.persistence.dao.IDAO;
+import com.macros.persistence.dao.constants.DBProviders;
+import com.macros.persistence.factory.DAOFactory;
+import com.macros.persistence.services.IPersistenceService;
+import com.macros.persistence.services.PersistenceService;
+
 public class App 
 {
     public static void main( String[] args )
     {
-        
+        final Optional<IDAO> dao = new DAOFactory().daoByProvider(DBProviders.MYSQL);
+        final IPersistenceService persistenceService = new PersistenceService(dao.get()); 
+        System.out.println(String.format("=====> IPersistanceService Instance: [%s] <=====", persistenceService));
     }
 
 }
